77f4232c963235f6b40041d0a6d7fe3b9bbdfb73
Tuigun Bekishev <tbekishev@gmail.com>
2022-10-26 20:41:22 -0300

    Assignment is done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    let newArr = [];
+    for (let i = 0; i < letters[0].length; i++){
+        newArr[i] = [];
+        for(let j = 0; j < letters.length; j++) {
+            newArr[i].push(letters[j][i])
+        }
     }
-}
 
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = newArr.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+}
 module.exports = wordSearch
